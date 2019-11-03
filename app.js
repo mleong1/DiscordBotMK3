154fae78496f9ccd6c4cb0bcb93bf9dbb2b15566
Matthew Leong <mattleong5@gmail.com>
2019-11-02 21:21:51 -0400

    First commit of JSON_Derulo

---
+++ app.js
@@ -0,0 +1,5 @@
+//const cannot be changed (final) var can be set again
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+bot.login("NjQwMzU1MTYyODM3MzUyNDU5.Xb4ogA.7RO6acAHlNgCn7gARwxOUfl9WJY")
