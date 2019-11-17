057b81a301d6d549a2436881892e5700783742fa
Matthew Leong <mattleong5@gmail.com>
2019-11-17 17:36:36 -0500

    Added functionality to deal community cards and determine a winner of the hand.

--- commands/poker/startGame.js
+++ commands/poker/startGame.js
@@ -3,6 +3,7 @@ const { TexasHoldEmPokerGameType, Player, PokerScoreService, Hand} = require('ty
 const deck = new TexasHoldEmPokerGameType().createDeck()
 const handScorer = new PokerScoreService()
 var communityCards = new Hand()
+deck.shuffle()
 
 class startTexasHoldEmCommand extends commando.Command{
     constructor(client){
@@ -20,7 +21,6 @@ class startTexasHoldEmCommand extends commando.Command{
         var players = []
         this.createPlayersFromVC(message, players)
         this.dealCards(message, players)
-        message.reply("What would you like to do: " + players[dealer] + "?")
         //fold, check, call, bet, or raise?
 
         //loop through all players for actions starting from the current dealer/small blind?
@@ -28,8 +28,12 @@ class startTexasHoldEmCommand extends commando.Command{
         //todo test loop with multiple people
         //todo add action to check hand and community cards
         //todo find a way to refactor and get this code into a method
+        //todo reset the deck after a game
         var responses = []
+        //console.log("Player number: " + players.length)
         for(var i = dealer; i < players.length; i ++) {
+            message.channel.send("What would you like to do: " + players[dealer] + "?")
+            console.log("Current iteration of the player loop: " + i)
             var response = await message.channel.awaitMessages(msg => {
                 //console.log(msg.author.username)
                 var args = msg.content.split(" ")
@@ -46,8 +50,16 @@ class startTexasHoldEmCommand extends commando.Command{
             }, {max: 1})
             responses.push(response)
         }
-        
+
         console.log("Done with the await input: " )
+
+        this.dealCommunityCards(message, communityCards, 3)
+
+        console.log("Community Cards dealt")
+
+        console.log(this.determineWinner(message, players, communityCards, handScorer))
+
+
     }
 
     createPlayersFromVC(message, players){
@@ -71,8 +83,31 @@ class startTexasHoldEmCommand extends commando.Command{
     dealCards(message, players){
         players.forEach((player) => {
             deck.deal(player.getHand(), 2)
-            message.reply(player.name + " has these cards: " + player.getHand().getCards())
+            //message.reply(player.name + " has these cards: " + player.getHand().getCards())
+            //send player private message of cards dealt to them
+            message.client.users.find("username", player.name).send("Here are your cards: " + player.getHand().getCards())
         });
+
+    }
+
+    dealCommunityCards(message, communityCards = [], num){
+        deck.deal(communityCards, num)
+        message.reply("These are the community cards: " + communityCards.getCards())
+    }
+
+    determineWinner(message, players, communityCards = [], handScorer) {
+        var winner = players[0].name
+        var highestScoringPlayerHand = 0
+        players.forEach((player) => {
+            var handValue = handScorer.scoreHand(player.hand, communityCards.getCards()).value
+            if(handValue > highestScoringPlayerHand){
+                winner = player.name
+                highestScoringPlayerHand = handValue
+            }
+        });
+        message.channel.send("The winner is: " + winner)
+        return winner
+
     }
 
 
